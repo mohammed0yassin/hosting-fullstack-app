739114fa1b52194493dea929b1852841dc811a1c
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2022-09-02 05:46:18 +0200

    fix deploy

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,3 @@
-aws s3 cp --recursive --acl public-read ./www s3://myfullstackappbucket/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myfullstackappbucket/
+aws s3 cp --recursive --acl public-read ./www s3://fullstackappbucket/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://fullstackappbucket/
 
