f5521073093b1c2553bd2315fbb884e06280144c
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2022-09-02 04:18:45 +0200

    env modify

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://myfullstackappelb-env.eba-mchmz2km.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
